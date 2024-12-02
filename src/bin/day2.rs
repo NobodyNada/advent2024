65e7320b0174d5de1ab009765a226b64c27ab633
Jonathan Keller <19418817+NobodyNada@users.noreply.github.com>
2024-12-01 21:26:08 -0800

    day2

--- src/bin/day2.rs
+++ src/bin/day2.rs
@@ -1,8 +1,35 @@
+#![feature(iter_map_windows)]
 pub use aoc_2024::prelude::*;
 
 fn main() -> Result<()> {
     let c = chal()?;
     let input = c.input.lines().map(Result::unwrap);
+    let input = input.map(|line| {
+        line.split_whitespace()
+            .map(|w| w.parse::<u32>().expect("parse error"))
+            .collect::<Vec<_>>()
+    });
+    let safe = input.filter(|input| {
+        let mut skippable = if c.part2 {
+            0..=input.len()
+        } else {
+            input.len()..=input.len() // invalid index
+        };
+
+        skippable.any(|i| {
+            let input = input
+                .iter()
+                .enumerate()
+                .filter_map(|(j, x)| if j == i { None } else { Some(x) });
+
+            (input.clone().is_sorted() || input.clone().rev().is_sorted())
+                && input
+                    .map_windows(|&x| x)
+                    .all(|[&a, &b]| (1..=3).contains(&a.abs_diff(b)))
+        })
+    });
+
+    println!("{}", safe.count());
 
     Ok(())
 }
